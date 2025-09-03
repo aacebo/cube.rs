0c267c19296556a3ee983f0bb90f0218bc3fbfab
aacebo <aacebowork@gmail.com>
2025-09-02 21:02:05 -0400

    Update wildcard.rs

--- cube-url/src/template/expression/wildcard.rs
+++ cube-url/src/template/expression/wildcard.rs
@@ -14,7 +14,11 @@ use crate::{
 /// `http://*/api/users`
 #[derive(Debug, Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct Wildcard(Asterisk);
+pub struct Wildcard {
+    left: Option<Box<Expression>>,
+    token: Asterisk,
+    right: Option<Box<Expression>>,
+}
 
 impl Wildcard {
     pub fn eval(&self, text: &str, url: &mut Url) -> Result<(), Error> {
@@ -29,12 +33,39 @@ impl Wildcard {
 
 impl Wildcard {
     pub fn parse(scan: &mut Scanner<'_>) -> Result<Expression, Error> {
-        return Ok(Expression::Wildcard(Self(Asterisk::parse(scan)?)));
+        let mut expr = Expression::parse(&mut *scan)?;
+
+        while scan.curr() == b'*' {
+            let op = Asterisk::parse(&mut *scan)?;
+            let mut right: Option<Box<Expression>> = None;
+
+            if !scan.is_eof() {
+                right = Some(Box::new(Expression::parse(&mut *scan)?));
+            }
+
+            expr = Expression::Wildcard(Self {
+                left: Some(Box::new(expr)),
+                token: Asterisk::parse(scan)?,
+                right,
+            });
+        }
+
+        return Ok(expr);
     }
 }
 
 impl fmt::Display for Wildcard {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        return write!(f, "{}", self.0);
+        if let Some(v) = &self.left {
+            write!(f, "{}", v)?;
+        }
+
+        write!(f, "{}", self.token);
+
+        if let Some(v) = &self.right {
+            write!(f, "{}", v)?;
+        }
+
+        return Ok(());
     }
 }
